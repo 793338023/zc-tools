319a3e5253fa59df334d8cc5f509978f779846fd
Mr.zhang <793338023@qq.com>
2021-12-15 22:33:31 +0800

    init

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 const { createProxyMiddleware } = require("http-proxy-middleware");
@@ -17,7 +15,6 @@ const proxy = createProxyMiddleware({
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use("/", proxy);
 
